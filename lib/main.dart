34d697161ccfa94049c5084666921ceb0b80f1e8
unknown <ssrah@Vivi.kcep.local>
2025-07-08 11:30:45 +0500

    Внесены изменения

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:gluvi/main.dart';
 
 void main() {
-Text('риает мир');
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+ }
+
+class _MyHomePageState extends State<MyHomePage>{
+  String displayText = 'Привеет!!';
+  Color btncolor = Colors.amber;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Главная страница'),),
+      body: Text(displayText),
+      floatingActionButton: FloatingActionButton(backgroundColor: btncolor ,child: Text('Нажми на меня!'), onPressed: (){
+        setState(() {
+          displayText = 'Как дела":)';
+          btncolor = btncolor == Colors.greenAccent ? Colors.amber : Colors.greenAccent;
+        });
+      }),
+    );
+  }
+}
+
+
+
+
